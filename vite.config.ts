13622d7dd6535951ff639b1a58791db3c67efe69
chibataku0815 <chiba@fores-tone.co.jp>
2024-08-04 19:46:47 +0900

    chore: Add initial project files and configurations

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import ssg from '@hono/vite-ssg'
+import mdx from '@mdx-js/rollup'
+import honox from 'honox/vite'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
+
+const entry = './app/server.ts'
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      honox(),
+      ssg({ entry }),
+      mdx({
+        jsxImportSource: 'hono/jsx',
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      }),
+    ],
+    css: {
+      postcss: {
+        plugins: [tailwindcss],
+      },
+    },
+  }
+})
